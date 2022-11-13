f0a8fb072f6dca252e64187d5d643f0230abd859
Bryant Biggs <bryantbiggs@gmail.com>
2022-11-13 10:25:30 -0500

    fix: Embed directory search script into action since files are not carried with the action (#15)

--- directories/directories.py
+++ directories/directories.py
@@ -2,16 +2,16 @@ import json
 import mmap
 import os
 import re
-from glob import glob
+import glob
 
 
 def get_directories():
     """
-    Return all Terraform (*.tf) files that contain the `required_version`
-      string, but does not contain `wrapper` in the path.
+    Return all Terraform (*.tf) files that contain the 'required_version'
+        string, but does not contain 'wrapper' in the path.
     """
 
-    terraform_files = glob('./**/*.tf', recursive=True)
+    terraform_files = glob.glob('./**/*.tf', recursive=True)
     directories = []
 
     for file in terraform_files:
@@ -21,9 +21,10 @@ def get_directories():
             with open(file, 'rb', 0) as rfile:
                 contents = mmap.mmap(rfile.fileno(), 0, access=mmap.ACCESS_READ)
                 if contents.find(b'required_version') != -1 and 'wrapper' not in file:
-                    directories.append(file)
+                    formatted = "/".join(file.split('/')[:-1])
+                    directories.append(formatted)
 
-    return json.dumps([x.replace('/versions.tf', '') for x in directories if not re.match(r'^.+/_', x)])
+    return json.dumps([x for x in directories if not re.match(r'^.+/_', x)])
 
 
 if __name__ == '__main__':
